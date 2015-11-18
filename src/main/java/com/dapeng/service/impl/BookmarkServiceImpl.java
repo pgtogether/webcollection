81e4d8c9e1460d7863bd3050c4037b8e20ba2335
1123529236 <1123529236@qq.com>
2015-11-18 22:52:43 +0800

    dfgd g

--- src/main/java/com/dapeng/service/impl/BookmarkServiceImpl.java
+++ src/main/java/com/dapeng/service/impl/BookmarkServiceImpl.java
@@ -1,6 +1,6 @@
 package com.dapeng.service.impl;
 
-import java.util.List;
+import java.util.List;  
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
