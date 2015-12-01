6457a56460a3fbf309099a356f01dea751557626
1123529236 <1123529236@qq.com>
2015-12-01 18:44:49 +0800

    咋回事呢

---
+++ src/main/java/com/dapeng/controller/form/BookMarkForm.java
@@ -0,0 +1,55 @@
+/**
+ * Project Name:webcollection
+ * File Name:BookMarkForm.java
+ * Package Name:com.dapeng.controller.form
+ * Date:2015年12月1日上午12:31:07
+ * Copyright (c) 2015, chenzhou1025@126.com All Rights Reserved.
+ *
+ */
+/**
+ * Project Name:webcollection
+ * File Name:BookMarkForm.java
+ * Package Name:com.dapeng.controller.form
+ * Date:2015年12月1日上午12:31:07
+ * Copyright (c) 2015, chenzhou1025@126.com All Rights Reserved.
+ *
+ */
+
+package com.dapeng.controller.form;
+
+/**
+ * ClassName:BookMarkForm <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2015年12月1日 上午12:31:07 <br/>
+ * 
+ * @author jiangdp
+ * @version
+ * @since JDK 1.6
+ * @see
+ */
+public class BookMarkForm {
+    
+    private String url;
+
+    private String bookmarkname;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getBookmarkname() {
+        return bookmarkname;
+    }
+
+    public void setBookmarkname(String bookmarkname) {
+        this.bookmarkname = bookmarkname;
+    }
+
+  
+
+}
---
+++ src/main/java/com/dapeng/service/bo/CategoryBO.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.dapeng.service.bo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class CategoryBO {
+
+}
